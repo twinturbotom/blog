8bce1bd0075ff744d43afba163d588503f367e79
twinturbotom <tommmello@gmail.com>
2012-11-06 14:27:33 -0500

    security on commetns

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :name, :title
-
+ 
   validates :name,  :presence => true
   validates :title, :presence => true,
                     :length => { :minimum => 5 }
-
-
-  has_many :comments
-
+  has_many :comments, :dependent => :destroy
 end
